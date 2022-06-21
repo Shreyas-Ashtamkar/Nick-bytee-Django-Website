b9e8842d65a4063ef11759a047b3cdbdc42ab99c
nick-bytee <nikhilkaushik2228@gmail.com>
2022-06-21 11:47:52 +0530

    Static Files Added

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,4 +122,6 @@ STATIC_DIRS = os.path.join(BASE_DIR,'static')
 
 STATIC_ROOT = os.path.join(BASE_DIR,'site_assets')
 # Default primary key field type
-# https://docs.djangoproject.
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
