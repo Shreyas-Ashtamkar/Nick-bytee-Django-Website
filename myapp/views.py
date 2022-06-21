ae9642e5bd0ead2ad2ebe5c8e88d8afb0a1701d7
nick-bytee <nikhilkaushik2228@gmail.com>
2022-06-21 14:13:00 +0530

    Configured Static Files

--- myapp/views.py
+++ myapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+def radio(request):
+    return render(request, 'radio1.html')
--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import mimetypes
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^3n#evtz(8@0onr-6()00o9%2&904vc*0r3!tx#fn+f=8wdxmj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,9 +120,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_DIRS = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = BASE_DIR / "static"
 
-STATIC_ROOT = os.path.join(BASE_DIR,'site_assets')
+mimetypes.add_type("text/css", ".css", True)
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
