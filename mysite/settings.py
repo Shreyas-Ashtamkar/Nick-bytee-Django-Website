13478122c8d88a58a9eb1d1b0484aa57d1984e5e
nick-bytee <nikhilkaushik2228@gmail.com>
2022-06-24 18:16:34 +0530

    try 2 for dynamic images

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,6 +126,9 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / "static"
 
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -133,5 +136,4 @@ STATIC_ROOT = BASE_DIR / "static"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'static')
-MEDIA_URL = '/media/'
+
